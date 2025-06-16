a56f811b72153170c03587ba4663ebddf5d6a495
Rahul chhajed <rahulchhajed215@gmail.com>
2025-06-16 21:44:11 +0530

    Update Tf.jsx
    changing uri for backend

--- src/COMPONENTS/Tf.jsx
+++ src/COMPONENTS/Tf.jsx
@@ -19,7 +19,7 @@ function Tf() {
             }
 
             const response = await axios.post(
-                "http://localhost:3000/check-title",
+                "https://fun-tutor-backend-production.up.railway.app/check-title",
                 {
                     title: sectionTitle,
                     quizTitle: quizTitle
